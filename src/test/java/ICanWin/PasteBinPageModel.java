209b4dcd89337d66c8165627486c516ff83355d7
Hayk Bakhshyan <hayk_bakhshyan@epam.com>
2021-06-16 15:04:15 +0400

    I can win task commit

---
+++ src/test/java/ICanWin/PasteBinPageModel.java
@@ -0,0 +1,60 @@
+package ICanWin;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class PasteBinPageModel {
+    private WebDriver driver;
+
+    @FindBy(css = "textarea[id='postform-text']")
+    private WebElement newPasteTextArea;
+
+    @FindBy(xpath = "//div[@class='form-group field-postform-expiration']/div/span[@class='select2 select2-container select2-container--default']")
+    private WebElement pasteExpirtaion;
+
+
+    @FindBy(css = "input[id='postform-name']")
+    private WebElement pastName;
+
+    @FindBy(css = "button[type='submit']")
+    private WebElement createNewPasteButton;
+
+    @FindBy(xpath = "//button[text()='AGREE']")
+    private WebElement agreeCookiesButton;
+
+    public PasteBinPageModel(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+
+    }
+
+    private void fillTextArea(String text) {
+        newPasteTextArea.sendKeys(text);
+    }
+
+    private void selectPasteExpiration(String pastExpirationValue) {
+        pasteExpirtaion.click();
+        driver.findElement(By.xpath("//li[text() ='" + pastExpirationValue + "']")).click();
+    }
+
+    private void fillPastName(String pastNameValue) {
+        pastName.sendKeys(pastNameValue);
+
+    }
+
+    private void agreeCookies() {
+        new WebDriverWait(driver, 30).until(ExpectedConditions.visibilityOf(agreeCookiesButton)).click();
+    }
+
+    public void submitForm(String text, String pastExpriationValue, String pastNameValue) {
+        agreeCookies();
+        fillTextArea(text);
+        selectPasteExpiration(pastExpriationValue);
+        fillPastName(pastNameValue);
+        createNewPasteButton.submit();
+
+    }
+}
---
+++ src/test/java/ICanWin/PasteBinTest.java
@@ -0,0 +1,32 @@
+package ICanWin;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class PasteBinTest {
+    private WebDriver driver;
+    private PasteBinPageModel pasteBinPageModel;
+
+    @BeforeTest(alwaysRun = true)
+    public void openBrowser() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://pastebin.com/");
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void submitForm() {
+        pasteBinPageModel = new PasteBinPageModel(driver);
+        pasteBinPageModel.submitForm("test", "10 Minutes", "Hayk");
+    }
+
+    @AfterTest(alwaysRun = true)
+    public void tearDown() {
+        driver.quit();
+
+    }
+}
